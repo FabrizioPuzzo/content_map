8c533538d4e3924cdc891283d66ca694ad7c3fd0
FabrizioPuzzo <fabriziopuzzo1@gmail.com>
2020-04-22 21:26:12 +0200

    Data_update 04/22/2020, 21:26:11

--- src/comp/InfoBox.js
+++ src/comp/InfoBox.js
@@ -75,31 +75,31 @@ class InfoBox extends Component {
               </thead>
               <tbody>
               <tr>
-                  <td>{this.props.data.entities[0]}</td>
+                  <td><a target="_blank" href= {this.props.data.google_hrefs[0]}>{this.props.data.entities[0]}</a></td>
                   <td>{this.props.data.entity_count[0]}</td>
                   <td>{this.props.data.fav_cnts[0]}</td>
                   <td>{(Math.round(this.props.data.sentiments[0] * 10) / 10).toFixed(2)}</td>
               </tr>
               <tr>
-                  <td>{this.props.data.entities[1]}</td>
+                  <td><a target="_blank" href= {this.props.data.google_hrefs[1]}>{this.props.data.entities[1]}</a></td>
                   <td>{this.props.data.entity_count[1]}</td>
                   <td>{this.props.data.fav_cnts[1]}</td>
                   <td>{(Math.round(this.props.data.sentiments[1] * 10) / 10).toFixed(2)}</td>
               </tr>
               <tr>
-                  <td>{this.props.data.entities[2]}</td>
+                  <td><a target="_blank" href= {this.props.data.google_hrefs[2]}>{this.props.data.entities[2]}</a></td>
                   <td>{this.props.data.entity_count[2]}</td>
                   <td>{this.props.data.fav_cnts[2]}</td>
                   <td>{(Math.round(this.props.data.sentiments[2] * 10) / 10).toFixed(2)}</td>
               </tr>
               <tr>
-                  <td>{this.props.data.entities[3]}</td>
+                  <td><a target="_blank" href= {this.props.data.google_hrefs[3]}>{this.props.data.entities[3]}</a></td>
                   <td>{this.props.data.entity_count[3]}</td>
                   <td>{this.props.data.fav_cnts[3]}</td>
                   <td>{(Math.round(this.props.data.sentiments[3] * 10) / 10).toFixed(2)}</td>
               </tr>
               <tr>
-                  <td>{this.props.data.entities[4]}</td>
+                  <td>{this.props.data.google_hrefs[4]}</td>
                   <td>{this.props.data.entity_count[4]}</td>
                   <td>{this.props.data.fav_cnts[4]}</td>
                   <td>{(Math.round(this.props.data.sentiments[4] * 10) / 10).toFixed(2)}</td>
